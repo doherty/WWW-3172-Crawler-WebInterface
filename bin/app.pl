f68580159b3c366242ba82c6719b37b92500c7d1
Mike Doherty <doherty@cs.dal.ca>
2011-09-23 22:59:28 -0300

    First stab at the interface

--- bin/app.pl
+++ bin/app.pl
@@ -1,4 +1,7 @@
 #!/usr/bin/env perl
+# PODNAME: dance
+# ABSTRACT: run your WWW::3172::Crawler::WebInterface application
+# VERSION
 use Dancer;
 use WWW::3172::Crawler::WebInterface;
 dance;
--- lib/WWW/3172/Crawler/WebInterface.pm
+++ lib/WWW/3172/Crawler/WebInterface.pm
@@ -1,10 +1,48 @@
 package WWW::3172::Crawler::WebInterface;
 use Dancer ':syntax';
-
-our $VERSION = '0.1';
+use Dancer::Plugin::DataFu;
+#use Template;
+use WWW::3172::Crawler;
+use HTML::Entities;
+# ABSTRACT: Provides a web frontend to WWW::3172::Crawler
+# VERSION
 
 get '/' => sub {
-    template 'index';
+    return template 'index.tt', { form => form->render('crawl', '/submit_crawl', 'crawl.url', 'crawl.max') };
+};
+
+post '/submit_crawl' => sub {
+    my $input = form;
+    if ($input->validate('crawl.url', 'crawl.max')) {
+        my $crawler = WWW::3172::Crawler->new(
+            host    => $input->params->{'crawl.url'},
+            max     => $input->params->{'crawl.max'},
+        );
+        my $data = $crawler->crawl;
+        my $table = '';
+        foreach my $url (keys %$data) {
+            no warnings 'uninitialized';
+            my $safe_url = encode_entities($url);
+            my $keywords = encode_entities($data->{$url}->{keywords});
+            my $description = encode_entities($data->{$url}->{description});
+            my $safe_size   = encode_entities($data->{$url}->{size});
+            my $safe_speed  = encode_entities(sprintf("%.5f", $data->{$url}->{speed}));
+
+            $table .= <<"TABLE";
+<tr>
+    <td><a href="$safe_url">$safe_url</a></td>
+    <td>$keywords</td>
+    <td>$description</td>
+    <td>$safe_size</td>
+    <td>$safe_speed</td>
+</tr>
+TABLE
+        }
+
+        return template 'table.tt', { table => $table };
+    }
+    redirect '/';
 };
 
 true;
+
