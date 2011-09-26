cbe8ed608b1d0b10b48b02e436f2729b4197c292
Mike Doherty <doherty@cs.dal.ca>
2011-09-26 20:00:30 -0300

    Form tweaks

--- profiles/crawl.pl
+++ profiles/crawl.pl
@@ -11,12 +11,18 @@ our $crawl = {
             my ($form, $field, $params) = @_;
             $form->error($field, $field->{error}) unless is_web_uri($field->{value});
         },
+        filters     => [qw(trim strip lowercase)],
     },
     max => {
         label       => 'Maximum number of pages to crawl:',
         required    => 0,
         value       => 5, # 50
         element     => { type => 'input_text' },
+        validation  => sub {
+            my ($form, $field, $params) = @_;
+            $form->error($field, 'Cannot crawl more than 200 pages at once') unless $field->{value} < 200;
+        },
+        filters     => [qw(trim strip numeric)],
     },
 };
 
