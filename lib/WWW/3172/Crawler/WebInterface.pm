1ce0c562339a2a60b839dfd6588c7901da29a97e
Mike Doherty <doherty@cs.dal.ca>
2011-09-29 18:12:28 -0300

    Add a list of the most popular keywords

--- lib/WWW/3172/Crawler/WebInterface.pm
+++ lib/WWW/3172/Crawler/WebInterface.pm
@@ -3,6 +3,7 @@ use Dancer ':syntax';
 use Dancer::Plugin::DataFu;
 #use Template;
 use WWW::3172::Crawler;
+use List::UtilsBy qw(nsort_by);
 use HTML::Entities qw(encode_entities);
 use Data::Table;
 # ABSTRACT: Provides a web frontend to WWW::3172::Crawler
@@ -15,31 +16,54 @@ get '/' => sub {
 post '/submit_crawl' => sub {
     my $input = form;
     if ($input->validate('crawl.url', 'crawl.max')) {
-        my $crawler = WWW::3172::Crawler->new(
+        my $crawler = $imported_crawler || WWW::3172::Crawler->new(
             host    => $input->params->{'crawl.url'},
             max     => $input->params->{'crawl.max'},
         );
         my $crawl_data = $crawler->crawl;
- 
-        my $headers = ['URL', 'Keywords', 'Description', 'Size (bytes)', 'Speed (s)'];
-        my @rows;
-        while (my ($url, $data) = each %$crawl_data) {
-            delete $crawl_data->{$url};
-            my $data = [
-                "<a href='$url'>" . encode_entities($url) . "</a>",
-                $data->{keywords},
-                $data->{description},
-                $data->{size},
-                sprintf("%.5f", $data->{speed}),
-            ];
-            push @rows, $data;
-        }
-        my $table = Data::Table->new(\@rows, $headers, 0);
 
-        return template 'table.tt', { table => $table->html };
+        return template 'table.tt', {
+            main_table  => _main_table($crawl_data),
+            stats_table => _stats_table($crawl_data),
+        };
     }
     redirect '/';
 };
+
+sub _main_table {
+    my $crawl_data = shift;
+
+    my $headers = ['URL', 'Keywords', 'Description', 'Size (bytes)', 'Speed (s)'];
+    my @rows;
+    while (my ($url, $data) = each %$crawl_data) {
+        push @rows, [
+            "<a href='$url'>" . encode_entities($url) . "</a>",
+            $data->{keywords},
+            $data->{description},
+            $data->{size},
+            sprintf("%.5f", $data->{speed}),
+        ];
+    }
+    return Data::Table->new(\@rows, $headers, 0)->html;
+}
+
+sub _stats_table {
+    my $crawl_data = shift;
+    my %keywords;
+
+    while (my ($url, $data) = each %$crawl_data) {
+        my @these_keywords = split /,\s?|\s+/, ($data->{keywords} || '');
+        $keywords{$_}++ for @these_keywords;
+    }
+    return '' if keys %keywords == 0;
+
+    my $html = '<ul><li>';
+    $html .= join '</li><li>', map { encode_entities($_) }
+        nsort_by { $keywords{$_} } keys %keywords;
+    $html .= '</li></ul>';
+
+    return $html;
+}
 
 true;
 
